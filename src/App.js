2d9141b2972af5ae2ad0d556f3d3661fcd373956
fengxinhou <fengxin.hou@thoughtworks.com>
2022-12-05 11:04:22 +0800

    feat: config route and login to register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import "./App.css";
 import Login from "./pages/Login/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/home";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
-    <div className="App">
-        <Login/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
