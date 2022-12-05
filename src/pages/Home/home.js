3554ac8787f80b9b8c5bb5602fb81b5f57307d64
fengxinhou <fengxin.hou@thoughtworks.com>
2022-12-05 16:44:57 +0800

    feat:home page basic style

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,7 +1,24 @@
 import React from "react";
-
+import "./home.css";
+import homeLogo from "../../assets/img/header-img.svg";
 function Home() {
-  return <div>首页</div>;
+  return (
+    <div className="home">
+      <header>
+        <img src={homeLogo} alt="logo" />
+        <h1>Kai's Blog</h1>
+      </header>
+      <div className="main">
+        <aside>
+          <div>数据概览</div>
+          <div>内容管理</div>
+          <div>发布文章</div>
+        </aside>
+        <main>主体部分</main>
+      </div>
+      <footer>底部</footer>
+    </div>
+  );
 }
 
 export default Home;
