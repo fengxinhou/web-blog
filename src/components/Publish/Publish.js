6ea43e69cd47076e39a23bb25b71b5837aac4ac6
fengxinhou <fengxin.hou@thoughtworks.com>
2022-12-06 16:13:09 +0800

    feat:publish article basic style

--- src/components/Publish/Publish.js
+++ src/components/Publish/Publish.js
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import "./publish.css";
+import { Link } from "react-router-dom";
 function Publish() {
-  return <div className="publish">发布文章</div>;
+  const [article, setArticle] = useState({
+    id: 0,
+    userId: 0,
+    url: "",
+    tag: "",
+    title: "",
+    content: "",
+    pubDate: "",
+    status: "",
+  });
+  const onFormUpdate = (key, value) => {
+    setArticle({
+      ...article,
+      [key]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    console.log(e);
+  };
+  return (
+    <div className="publish">
+      <div className="publish_content">
+        <div className="publish_header">
+          <Link to={"/"}>首页</Link>
+          &nbsp;>&nbsp;
+          <span>发布文章</span>
+        </div>
+        <form onSubmit={handleSubmit} className="publish_form">
+          <div className="article_title">
+            <label>
+              标题：
+              <input
+                type="text"
+                placeholder="请输入文章标题"
+                value={article.title}
+                onChange={(e) => {
+                  onFormUpdate("name", e.target.value);
+                }}
+              />
+            </label>
+          </div>
+          <div className="article_category">
+            <label>
+              分类：
+              <section>
+                <option></option>
+              </section>
+            </label>
+          </div>
+          <div className="article_pic">
+            <label>
+              封面：
+              <input
+                type="text"
+                value={article.url}
+                onChange={(e) => {
+                  onFormUpdate("name", e.target.value);
+                }}
+              />
+            </label>
+          </div>
+          <div className="article_content">
+            <label>
+              内容：
+              <input
+                type="text"
+                value={article.content}
+                onChange={(e) => {
+                  onFormUpdate("name", e.target.value);
+                }}
+              />
+            </label>
+          </div>
+          <div className="article_button">
+            <button type="submit">
+              <span>发布文章</span>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Publish;
