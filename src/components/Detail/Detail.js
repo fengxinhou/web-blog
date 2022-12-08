2b9a264ae03bbec3903a172670a7c2552cdd86fa
fengxinhou <fengxin.hou@thoughtworks.com>
2022-12-08 14:41:39 +0800

    feat:render article detail

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./detail.css";
+import { observer } from "mobx-react-lite";
+import { useStore } from "../../store";
+
 function Detail() {
+  const { articleStore } = useStore();
+  useEffect(() => {
+    articleStore.getArticleDetail().then();
+  });
+  const { article } = articleStore;
+  const GiveThumbs = () => {
+    article.thumbsUp = article.thumbsUp + 1;
+    //发送请求，修改点赞数
+    console.log(article.thumbsUp);
+  };
   return (
     <div className="detail">
-      <div className="detail_header">
-        <span>文章详情</span>
-      </div>
+      {
+        <>
+          <div className="detail_header">
+            <div className="detail_meta">
+              <span>创建时间</span>
+              <time>{article.creationTime}</time>
+            </div>
+            <h2 className="detail_title">{article.title}</h2>
+          </div>
+          <section className="detail_content">{article.description}</section>
+          <div className="detail_remark">
+            <span>作者：{article.author.name}</span>
+            <div className="thumbs" type="button" onClick={GiveThumbs}>
+              <i className="iconfont">&#xe65d;</i>
+              <span>点赞</span>
+            </div>
+          </div>
+          <footer className="switch_article">
+            <button>上一篇</button>
+            <button>下一篇</button>
+          </footer>
+        </>
+      }
     </div>
   );
 }
 
-export default Detail;
+export default observer(Detail);
--- src/store/articleStore.js
+++ src/store/articleStore.js
@@ -1,10 +1,19 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
+import axios from "axios";
 
+const URL = "http://localhost:3000/article";
 class ArticleStore {
-  article = [];
+  article = {};
   constructor() {
     makeAutoObservable(this);
   }
+  getArticleDetail = async () => {
+    const res = await axios.get(URL);
+    runInAction(() => {
+      this.article = res.data;
+    });
+    return this.article;
+  };
 }
 
 export default ArticleStore;
