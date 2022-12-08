f85569daa82df980abb8c0e825d722cb15aaadac
fengxinhou <fengxin.hou@thoughtworks.com>
2022-12-08 15:16:40 +0800

    feat: add highLights to menu

--- src/pages/Frame/Frame.js
+++ src/pages/Frame/Frame.js
@@ -3,9 +3,10 @@ import "./frame.css";
 import homeLogo from "../../assets/img/header-img.svg";
 import { useStore } from "../../store";
 import { observer } from "mobx-react-lite";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 function Frame() {
   const { menuStore } = useStore();
+  const { pathname } = useLocation();
   useEffect(() => {
     menuStore.getMenu().then();
   }, [menuStore]);
@@ -13,14 +14,16 @@ function Frame() {
     <div className="frame">
       <header>
         <img src={homeLogo} alt="logo" />
-        <h1>Kai's Blog</h1>
+        <h1>Web Blog</h1>
       </header>
       <div className="main">
         <aside>
           {menuStore.menu.map((item) => (
-            <div key={item.path} className="menuItem">
-              <i className="iconfont">{item.icon}</i>
-              <Link to={item.path}>{item.title}</Link>
+            <div className={pathname === item.path ? "highLight" : null}>
+              <div key={item.path} className="menuItem">
+                <i className="iconfont">{item.icon}</i>
+                <Link to={item.path}>{item.title}</Link>
+              </div>
             </div>
           ))}
         </aside>
