27619a956bf76c6e0db5832ef5f49c327ea99389
fengxinhou <fengxin.hou@thoughtworks.com>
2022-12-27 11:29:33 +0800

    feat:render userInformation

--- src/pages/Frame/Frame.js
+++ src/pages/Frame/Frame.js
@@ -1,22 +1,35 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./frame.css";
 import mainBg from "../../assets/img/mainBg.jpg";
-import { useStore } from "../../store";
-import { observer } from "mobx-react-lite";
 import { Link, Outlet, useLocation } from "react-router-dom";
+import { getToken } from "../../utils";
 function Frame() {
-  const { menuStore, blogListStore } = useStore();
   const { pathname } = useLocation();
-  useEffect(() => {
-    menuStore.getMenuList().then();
-  }, [menuStore, blogListStore]);
-
+  const parseToken = () => {
+    const token = getToken();
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    return decodeURIComponent(
+      window
+        .atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+  };
+  const blogMenu = JSON.parse(parseToken());
   return (
     <div className="frame">
       <img src={mainBg} alt="mainBg" />
       <div className="main">
         <div className="navBar">
-          {menuStore.menuList.map((item) => (
+          <div className="userInfo">
+            <i className="iconfont">&#xe64d;</i>
+            <span>{blogMenu.name}</span>
+          </div>
+          {blogMenu.permission.map((item) => (
             <div key={item.name} className="menuItem">
               <div className={pathname === item.path ? "highLight" : null}>
                 <Link to={item.path}>{item.title}</Link>
@@ -32,4 +45,4 @@ function Frame() {
   );
 }
 
-export default observer(Frame);
+export default Frame;
--- src/store/menuStore.js
+++ src/store/menuStore.js
@@ -1,29 +1,41 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import { getToken } from "../utils";
+
 class MenuStore {
   menuList = [];
+  userInfoName = "";
   token = getToken();
   constructor() {
     makeAutoObservable(this);
   }
-  getMenuList = async () => {
-    setTimeout(() => {
-      const base64Url = this.token.split(".")[1];
-      const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-      const jsonPayload = decodeURIComponent(
-        window
-          .atob(base64)
-          .split("")
-          .map(function (c) {
-            return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-          })
-          .join("")
-      );
-      runInAction(() => {
-        this.menuList = JSON.parse(jsonPayload).permission;
-      });
-    }, 100);
+  parseToken() {
+    const base64Url = this.token.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    return decodeURIComponent(
+      window
+        .atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+  }
+  // jsonPayload = this.parseToken();
+  blogMenu = JSON.parse(this.parseToken());
+  getMenuList = () => {
+    runInAction(() => {
+      this.menuList = this.blogMenu.permission;
+    });
     return this.menuList;
+  };
+
+  getUserInfo = () => {
+    runInAction(() => {
+      this.userInfoName = this.blogMenu.name;
+      console.log(this.userInfoName);
+    });
+    return this.userInfoName;
   };
 }
 
